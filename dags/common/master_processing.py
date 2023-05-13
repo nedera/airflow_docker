d462bac256d91bde06391633a838301232480b8c
Nedera <nghieped@gmail.com>
2023-05-14 00:47:46 +0700

    Revert "Revert "ETL for Master File""
    This reverts commit df0088a8d96aaf5e7a393e976f2c2a548221a7de.

--- dags/common/master_processing.py
+++ dags/common/master_processing.py
@@ -1,9 +1,8 @@
 import re
 def rename_cols(col):
-    # Chuyển đổi tất cả các ký tự thành chữ thường
-    col = col.lower()
-    # Thay thế các dấu cách, /, - bằng dấu gạch dưới (_)
-    col = re.sub(r'[\s/-]+', '_', col)
+    col=col.lower()
     # Viết hoa chữ cái đầu của từ
-    col = re.sub(r'\b\w', lambda x: x.group().upper(), col)
+    col = re.sub(r'\b(\w)|[-/](\w)', lambda x: x.group().upper(), col)
+    # Thay thế các dấu cách, /, - bằng dấu gạch dưới (_)
+    col = re.sub(r'[\s/-]+', '', col)
     return col
