68384727ac97c12039552f64755844d3448d095a
Nedera <nghieped@gmail.com>
2023-05-14 00:46:53 +0700

    Revert "ETL for Master File"
    This reverts commit 131ced4a4fa523d0bf9ad2e2208ae5c1c3771f9e.

--- dags/common/master_processing.py
+++ dags/common/master_processing.py
@@ -1,8 +1,9 @@
 import re
 def rename_cols(col):
-    col=col.lower()
-    # Viết hoa chữ cái đầu của từ
-    col = re.sub(r'\b(\w)|[-/](\w)', lambda x: x.group().upper(), col)
+    # Chuyển đổi tất cả các ký tự thành chữ thường
+    col = col.lower()
     # Thay thế các dấu cách, /, - bằng dấu gạch dưới (_)
-    col = re.sub(r'[\s/-]+', '', col)
+    col = re.sub(r'[\s/-]+', '_', col)
+    # Viết hoa chữ cái đầu của từ
+    col = re.sub(r'\b\w', lambda x: x.group().upper(), col)
     return col
--- dags/master_etl.py
+++ dags/master_etl.py
@@ -1,5 +1,6 @@
 import time
-from extract.master_extract import *
+
+from common.master_processing import rename_cols
 from datetime import datetime, timedelta
 from airflow.models.dag import DAG
 from airflow.decorators import task
@@ -8,43 +9,29 @@ from airflow.hooks.base_hook import BaseHook
 import pandas as pd
 from sqlalchemy import create_engine
 
-def master_load(df: pd.DataFrame, name_table: str, engine):
-    path = './dags/public/dataWarehouse/master'
-    df.to_csv(f'{path}/{name_table}.csv', index=False)
 
+MASTER_DATA = {
+    'Ddeployment Master': [],
+    'SKUMaster': [],
+    'LocationMaster': [],
+    'PivotalSizeMaster': [],
+    'Seasonality Master': [],
+}
 
-    df.to_sql(f'{name_table}', engine, if_exists='replace', index=False)
-
+# Extract tasks
 @task()
-def deployment_table_etl(engine):
-    deploy_df = DeploymentExt.get_dataframe()
-    master_load(deploy_df, 'stg_DeploymentMaster', engine)
-    return {'Table(s) processed': 'stg_DeploymentMaster imported successfull'}
+def get_src_table():
+    import os
+    print(f'Hello: >>{os.getcwd()}')
+    path = '/opt/airflow/dags/public/dataLake/master'
+    for table in MASTER_DATA.keys():
 
-@task()
-def sku_master_table_etl(engine):
-    sku_master_df = SkuMasterExt.get_dataframe()
-    master_load(sku_master_df, 'stg_SkuMaster', engine)
-    return {'Table(s) processed': 'stg_SkuMaster imported successfull'}
-
-@task()
-def location_table_etl(engine):
-    location_df = LocationExt.get_dataframe()
-    master_load(location_df, 'stg_LocationMaster', engine)
-    return {'Table(s) processed': 'stg_LocationMaster imported successfull'}
-
-@task()
-def pivotal_size_table_etl(engine):
-    pivotal_size_df = PivotalSizeExt.get_dataframe()
-    master_load(pivotal_size_df, 'stg_PivotalSizeMaster', engine)
-    return {'Table(s) processed': 'stg_PivotalSizeMaster imported successfull'}
-
-@task()
-def seasonality_table_etl(engine):
-    seasonality_df = SeasonalityExt.get_dataframe()
-    master_load(seasonality_df, 'stg_SeasonalityMaster', engine)
-    return {'Table(s) processed': 'stg_SeasonalityMaster imported successfull'}
-
+        df = pd.read_csv(f'{path}/{table}.csv')
+        print('===+++===')
+        print(df.columns)
+        df = rename_columns(df)
+        print(df.columns)
+        print('===+++===')
 
 default_args = {
     'owner': 'NederaKUN',
@@ -61,11 +48,8 @@ with DAG(
     catchup=False,
     # tags=
 ) as dag:
-    conn = BaseHook.get_connection('vector_db')
-    engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
     with TaskGroup("extract_master_load", tooltip="Extract and load source data") as extract_load_src:
-        # src_table = deployment_table_etl()
-        [deployment_table_etl(engine), sku_master_table_etl(engine), location_table_etl(engine),
-         pivotal_size_table_etl(engine), seasonality_table_etl(engine)]
+        src_table = get_src_table()
+        [src_table]
     extract_load_src
 
